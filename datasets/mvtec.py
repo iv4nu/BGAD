96d247d1bb71a83ec8d61914b8b91fe636c6da9b
Ivan prisco <ivanprisco@MBP-di-Ivan.station>
2025-05-02 13:25:14 +0200

    Fix: use pretrained EfficientNet-B6 from local .pth, force standard engine

--- datasets/mvtec.py
+++ datasets/mvtec.py
@@ -16,9 +16,9 @@ from skimage.segmentation import mark_boundaries
 
 
 # URL = 'ftp://guest:GU.205dldo@ftp.softronics.ch/mvtec_anomaly_detection/mvtec_anomaly_detection.tar.xz'
-MVTEC_CLASS_NAMES = ['bottle']#, 'cable', 'capsule', 'carpet', 'grid',
-               #'hazelnut', 'leather', 'metal_nut', 'pill', 'screw',
-               #'tile', 'toothbrush', 'transistor', 'wood', 'zipper']
+MVTEC_CLASS_NAMES = ['bottle', 'cable', 'capsule', 'carpet', 'grid',
+               'hazelnut', 'leather', 'metal_nut', 'pill', 'screw',
+               'tile', 'toothbrush', 'transistor', 'wood', 'zipper']
 
 
 class MVTecDataset(Dataset):
