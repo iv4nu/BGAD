888a9bd0b0f9c9f90b4cf1bcb3bb3b09fa45cb3b
Ivan prisco <ivanprisco@MacBookPro.station>
2025-05-19 15:54:05 +0200

    Addestramento senza augumentation della classe bottle

--- engines/bgad_train_engine.py
+++ engines/bgad_train_engine.py
@@ -181,6 +181,66 @@ def validate(args, epoch, data_loader, encoder, decoders):
 
     return img_auc, pix_auc, pix_pro
 
+def validate2(args, data_loader, encoder, decoders):
+    print('\nCompute loss and scores on category: {}'.format(args.class_name))
+    
+    decoders = [decoder.eval() for decoder in decoders]
+    
+    image_list, gt_label_list, gt_mask_list, file_names, img_types = [], [], [], [], []
+    logps_list = [list() for _ in range(args.feature_levels)]
+    with torch.no_grad():
+        for i, (image, label, mask, file_name, img_type) in enumerate(tqdm(data_loader)):
+            if args.vis:
+                image_list.extend(t2np(image))
+                file_names.extend(file_name)
+                img_types.extend(img_type)
+            gt_label_list.extend(t2np(label))
+            gt_mask_list.extend(t2np(mask))
+            
+            image = image.to(args.device) # single scale
+            features = encoder(image)  # BxCxHxW
+            for l in range(args.feature_levels):
+                e = features[l]  # BxCxHxW
+                bs, dim, h, w = e.size()
+                e = e.permute(0, 2, 3, 1).reshape(-1, dim)
+               
+                # (bs, 128, h, w)
+                pos_embed = positionalencoding2d(args.pos_embed_dim, h, w).to(args.device).unsqueeze(0).repeat(bs, 1, 1, 1)
+                pos_embed = pos_embed.permute(0, 2, 3, 1).reshape(-1, args.pos_embed_dim)
+                decoder = decoders[l]
+
+                if args.flow_arch == 'flow_model':
+                    z, log_jac_det = decoder(e)  
+                else:
+                    z, log_jac_det = decoder(e, [pos_embed, ])
+
+                logps = get_logp(dim, z, log_jac_det)  
+                logps = logps / dim  
+                logps_list[l].append(logps.reshape(bs, h, w))
+    
+    scores = convert_to_anomaly_scores(args, logps_list)
+    # calculate detection AUROC
+    img_scores = np.max(scores, axis=(1, 2))
+    gt_label = np.asarray(gt_label_list, dtype=bool)
+    #img_auc = roc_auc_score(gt_label, img_scores)
+    img_auc = -1
+    # calculate segmentation AUROC
+    gt_mask = np.squeeze(np.asarray(gt_mask_list, dtype=bool), axis=1)
+    pix_auc = roc_auc_score(gt_mask.flatten(), scores.flatten())
+
+    pix_pro = -1
+    if args.pro:
+        pix_pro = calculate_pro_metric(scores, gt_mask)
+    
+    if args.vis:
+        img_threshold, pix_threshold = evaluate_thresholds(gt_label, gt_mask, img_scores, scores)
+        save_dir = os.path.join(args.output_dir, args.exp_name, 'vis_results', args.class_name)
+        save_dir = os.path.join('vis_results', args.class_name)
+        os.makedirs(save_dir, exist_ok=True)
+        plot_visualizing_results(image_list, scores, img_scores, gt_mask_list, pix_threshold, 
+                                 img_threshold, save_dir, file_names, img_types)
+
+    return img_auc, pix_auc, pix_pro
 
 def train(args):
     # Feature Extractor
@@ -246,23 +306,12 @@ def train(args):
         # === Validazione finale su test set ===
     
     print("\n[Post-Training Evaluation] Eseguo validazione finale sul test set...")
-    #solo il test loader che già lo restituisce create_fas_data_loader
+  
     encoder.eval()
     decoders = [decoder.eval() for decoder in decoders]
 
-    img_auc, pix_auc, pix_pro = validate(args, args.meta_epochs - 1, test_loader, encoder, decoders)
-
-    print(f"[FINAL] {args.class_name} Image AUC: {img_auc * 100:.2f}")
-    print(f"[FINAL] {args.class_name} Pixel AUC: {pix_auc * 100:.2f}")
-    print(f"[FINAL] {args.class_name} Pixel PRO: {pix_pro * 100:.2f}")
-    
-    print("\n[ 2 Post-Training Evaluation] Eseguo validazione finale sul test set...")
-    #solo il test loader che già lo restituisce create_fas_data_loader
-    encoder.eval()
-    decoders = [decoder.eval() for decoder in decoders]
-
-    test_loader=create_test_data_loader()
-    img_auc, pix_auc, pix_pro = validate(args, args.meta_epochs - 1, test_loader, encoder, decoders)
+    test_loader=create_test_data_loader(args)
+    img_auc, pix_auc, pix_pro = validate2(args, test_loader, encoder, decoders)
 
     print(f"[FINAL] {args.class_name} Image AUC: {img_auc * 100:.2f}")
     print(f"[FINAL] {args.class_name} Pixel AUC: {pix_auc * 100:.2f}")
@@ -270,3 +319,4 @@ def train(args):
     
 
     return img_auc_obs.max_score, pix_auc_obs.max_score, pix_pro_obs.max_score
+
